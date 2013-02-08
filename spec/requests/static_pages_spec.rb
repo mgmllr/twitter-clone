fb8654bf6684efb1385b1b9804443a7016763a8f
Max Miller <maxwellgmiller@gmail.com>
2013-02-08 11:55:00 -0500

    Refactor Specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,7 +9,6 @@ describe "Static pages" do
     it { should have_selector('title', text: full_title(page_title)) }
   end
 
-
   describe "Home page" do
     before { visit root_path }
     
@@ -18,7 +17,7 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
     it { should_not have_selector('title', :text => "| Home" ) }
- 
+  
   end
 
   describe "Help page" do
@@ -49,5 +48,20 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign Up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
   end
 end
